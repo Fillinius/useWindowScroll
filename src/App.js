d5dcd68b9b68768075ed8b0c2c6bb5ee3ca9cf1c
Vladimir Revyagin <skorabeyf@gmail.com>
2025-01-20 12:42:20 +0300

    feat:useWindowScroll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg'
 import './App.css'
+import { useWindowScroll } from './useWindowScroll'
 
 function App() {
+  const [scroll, scrollTo] = useWindowScroll()
+  console.log(scroll)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div>
+        <p>
+          Scroll position x: {scroll.x}, y: {scroll.y}
+        </p>
+        <button onClick={() => scrollTo({ y: 0 })}>Scroll to top</button>
+      </div>
     </div>
   )
 }
